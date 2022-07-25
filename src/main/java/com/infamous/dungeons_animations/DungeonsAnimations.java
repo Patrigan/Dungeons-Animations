485a24572cec7738c29c419dc0e1c9e25f586e86
pprovinciael <patrick.provinciael@dpgmedia.be>
2022-07-25 09:04:20 +0200

    Initial commit

---
+++ src/main/java/com/infamous/dungeons_animations/DungeonsAnimations.java
@@ -0,0 +1,48 @@
+package com.infamous.dungeons_animations;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(DungeonsAnimations.MODID)
+public class DungeonsAnimations
+{
+    // Directly reference a log4j logger.
+    public static final String MODID = "dungeons_animations";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+
+    public DungeonsAnimations() {
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        // Register the processIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
